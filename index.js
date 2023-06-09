6a4cf883d79638d0f6f024c13458cb3f7e724cce
Saiful264 <saifulislamx264@gmail.com>
2023-06-09 21:04:34 +0600

    jwt token been added

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+const varifyJwt = (req, res, next)=>{
+  const authorization = req.header.authorization;
+  if (!authorization) {
+    res.status(401).send({erro: true, message: "unauthorized access"})
+  }
 
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+    if(err){
+      res.status(401).send({ error: true, message: "unauthorized access" });
+    }
+
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -27,8 +43,36 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
     // write code here
+    await client.connect();
+
+    const userCollection = client.db('summerCampdb').collection('user');
+
+    console.log(process.env.ACCESS_TOKEN_SECRET);
+
+    app.post("/jwt", (req, res)=>{
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn: "1h"
+      });
+
+      res.send({token});
+    })
+
+    app.post('/users', async(req, res)=>{
+      const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
 
     app.get("/", (req, res) => {
       res.send("Summer Camp Server is running..");
